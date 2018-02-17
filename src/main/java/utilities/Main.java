035fc1b5c683692682fcf585042c9f1fe13696d7
A.R. Maged <abdulrahman.maged95@gmail.com>
2018-02-17 02:01:52 +0200

    :sparkles: Implement isUUID() utility function

---
+++ src/main/java/utilities/Main.java
@@ -0,0 +1,9 @@
+package utilities;
+
+public class Main {
+    public static boolean isUUID(String text) {
+        return text.matches(
+                "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$"
+        );
+    }
+}
