a67ef9f848d230fadc25ced1c3349b4f514ccf06
A.R. Maged <abdulrahman.maged95@gmail.com>
2018-02-18 00:09:09 +0200

    :sparkles: Implement generateUUID() utility function

--- src/main/java/utilities/Main.java
+++ src/main/java/utilities/Main.java
@@ -4,12 +4,17 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.UUID;
 
 public class Main {
     public static boolean isUUID(String text) {
         return text.matches(
                 "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$"
         );
+    }
+
+    public static String generateUUID() {
+        return UUID.randomUUID().toString();
     }
 
     public static Properties readPropertiesFile(String filePath) throws IOException {
