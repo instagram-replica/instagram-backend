e279b0104e3b81e492dd84d8037f62e74e22adea
A.R. Maged <abdulrahman.maged95@gmail.com>
2018-02-17 18:04:35 +0200

    :wrench: Place PostgreSQL connection params in config file

--- src/main/java/persistence/sql/Main.java
+++ src/main/java/persistence/sql/Main.java
@@ -2,15 +2,19 @@ package persistence.sql;
 
 import org.javalite.activejdbc.Base;
 
-import static persistence.sql.Configuration.*;
+import java.io.IOException;
+import java.util.Properties;
+
+import static utilities.Main.readPropertiesFile;
 
 public class Main {
-    public static void openConnection() {
+    public static void openConnection() throws IOException {
+        Properties properties = readPropertiesFile("src/main/resources/config.properties");
         Base.open(
-                DATABASE_DRIVER,
-                DATABASE_URL,
-                DATABASE_USER,
-                DATABASE_PASSWORD
+                properties.getProperty("POSTGRESQL_DRIVER"),
+                properties.getProperty("POSTGRESQL_URL"),
+                properties.getProperty("POSTGRESQL_USER"),
+                properties.getProperty("POSTGRESQL_PASSWORD")
         );
     }
 
