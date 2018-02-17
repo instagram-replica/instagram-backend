05f30fe955972afd40d54ca9c6bda0c66577bcd9
A.R. Maged <abdulrahman.maged95@gmail.com>
2018-02-17 02:01:16 +0200

    :wrench: Specify PostgreSQL connection parameters

---
+++ src/main/java/persistence/sql/Configuration.java
@@ -0,0 +1,8 @@
+package persistence.sql;
+
+class Configuration {
+    static final String DATABASE_DRIVER = "org.postgresql.Driver";
+    static final String DATABASE_URL = "jdbc:postgresql://localhost:5432/instagram_development";
+    static final String DATABASE_USER = "<user>";
+    static final String DATABASE_PASSWORD = "<password>";
+}
--- src/main/java/persistence/sql/Main.java
+++ src/main/java/persistence/sql/Main.java
@@ -1,4 +1,20 @@
 package persistence.sql;
 
+import org.javalite.activejdbc.Base;
+
+import static persistence.sql.Configuration.*;
+
 public class Main {
+    public static void openConnection() {
+        Base.open(
+                DATABASE_DRIVER,
+                DATABASE_URL,
+                DATABASE_USER,
+                DATABASE_PASSWORD
+        );
+    }
+
+    public static void closeConnection() {
+        Base.close();
+    }
 }
