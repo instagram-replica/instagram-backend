26cfc64c5ebd77aadb6c5833f7bd3a0b1f8c590b
Abdelrahman Hussein <abdokhaled30@hotmail.com>
2018-02-19 21:45:49 +0200

    sign in

--- src/main/java/services/users/Authentication.java
+++ src/main/java/services/users/Authentication.java
@@ -1,4 +1,22 @@
 package services.users;
 
+import org.json.JSONObject;
+import persistence.sql.users.Main;
+import persistence.sql.users.User;
+
 public class Authentication {
+
+    public static JSONObject SignIn(JSONObject paramsoject){
+        String name=paramsoject.getString("username");
+        String password=paramsoject.getString("passwordHash");
+        User user = Main.getUserById(name);
+        if(user.getPasswordHash().equals(password)){
+            JSONObject response = new JSONObject();
+            JSONObject item = new JSONObject();
+            item.put("sessionId", 12);
+            response.put("response", item);
+            return response;
+        }else
+            return new JSONObject().put("error", "null");
+    }
 }
