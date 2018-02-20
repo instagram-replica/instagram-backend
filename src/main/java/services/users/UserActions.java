2855e29b99a617ef84ee9ae657598be0b592d201
Yasmeen Wafa <yasmeenwafa1995@gmail.com>
2018-02-20 13:41:50 +0200

    about to finalize update profile, checking if operation was successful is still missing

--- src/main/java/services/users/UserActions.java
+++ src/main/java/services/users/UserActions.java
@@ -1,10 +1,12 @@
 package services.users;
 
 import org.json.JSONObject;
+import persistence.sql.users.Gender;
 import persistence.sql.users.Main;
 import persistence.sql.users.User;
 
 import java.sql.Date;
+import java.time.Instant;
 
 public class UserActions {
     public static JSONObject CreateFollow(JSONObject paramsObject, String loggedInUserId) {
@@ -31,15 +33,15 @@ public class UserActions {
 
         return jObject;
     }
-    public static JSONObject DeleteUser(JSONObject paramsObject)
+    public static JSONObject DeleteUser(JSONObject paramsObject, String loggedInUserId)
     {
         JSONObject jObject = new JSONObject();
         JSONObject inner = new JSONObject();
-        String userId = paramsObject.getString("userId");
+//        String userId = paramsObject.getString("userId");
 
         //@stub
-        User deletedUser  = Main.deleteUser(userId);
-        inner.put("id",userId);
+        User deletedUser  = Main.deleteUser(loggedInUserId);
+        inner.put("id",loggedInUserId);
         jObject.put("user", inner);
         if(deletedUser.getDeletedAt()!=null)
         {
@@ -51,4 +53,36 @@ public class UserActions {
         return jObject;
 
     }
+    public static JSONObject UpdateProfile(JSONObject paramsObject, String loggedInUserId)
+    {
+        JSONObject jObject = new JSONObject();
+        JSONObject inner = new JSONObject();
+
+        String blob = paramsObject.getString("blob");
+        String name = paramsObject.getString("name");
+        String username = paramsObject.getString("username");
+        String website = paramsObject.getString("website");
+        String bio = paramsObject.getString("bio");
+        String email = paramsObject.getString("email");
+        String phone = paramsObject.getString("phone");
+        String gender = paramsObject.getString("gender");
+
+        User user = new User();
+        user.setFullName(name);
+        user.setUsername(username);
+        user.setWebsiteUrl(website);
+        user.setBio(bio);
+        user.setEmail(email);
+        user.setPhoneNumber(phone);
+        user.setGender(Gender.valueOf(gender));
+        jObject.put("data", paramsObject);
+        user = Main.updateUser(loggedInUserId,user);
+        jObject.put("error","null");
+        //must validate if update was successful
+//        if(user.getUpdatedAt().before(Date.valueOf()))
+
+//        jObject.put("error",error);
+        return jObject;
+    }
+
 }
