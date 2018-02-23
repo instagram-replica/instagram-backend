a45c5c9f44f9b5d85d194c9ca61fb331d0316bf7
Mohamed Shokr <mohamedshokrr@gmail.com>
2018-02-23 17:33:21 +0200

    continuing working on the app, awaiting ArangoInterface modifications

--- src/main/java/services/activities/ActivityActions.java
+++ src/main/java/services/activities/ActivityActions.java
@@ -8,7 +8,7 @@ public class ActivityActions {
 	
 	public static void handlePostLikeActivity(JSONObject requestJSON, String userId) {	
 		JSONObject params = requestJSON.getJSONObject("params");
-		String postId = params.getString("postId");
+		String recieverId = params.getString("recieverId");
 		
 		// create a new json object required to save the follow activity in the database
 		// by calling ArangoInterfaceMethods.insertActivity 
@@ -17,14 +17,28 @@ public class ActivityActions {
 		
 		JSONObject activityJSON = new JSONObject();
 		activityJSON.put("activity_type", "liking_post");
-		activityJSON.put("post_id", postId);
 		activityJSON.put("sender_id", userId);
+		activityJSON.put("reciever_id", recieverId);
 		
 		ArangoInterfaceMethods.insertActivity(activityJSON);
 		
 	}
 	
-	public static void handleCommentLikeActivity(JSONObject activityJSON, String userId) {	
+	public static void handleCommentLikeActivity(JSONObject requestJSON, String userId) {
+		JSONObject params = requestJSON.getJSONObject("params");
+		String recieverId = params.getString("recieverId");
+		
+		// create a new json object required to save the follow activity in the database
+		// by calling ArangoInterfaceMethods.insertActivity 
+		
+		// [requestJSON] ----> this controller -----> [ArangoInterface] 
+		
+		JSONObject activityJSON = new JSONObject();
+		activityJSON.put("activity_type", "liking_comment");
+		activityJSON.put("sender_id", userId);
+		activityJSON.put("reciever_id", recieverId);
+		
+		ArangoInterfaceMethods.insertActivity(activityJSON);
 	}
 	
 	public static void handleFollowActivity(JSONObject activityJSON, String userId) {
