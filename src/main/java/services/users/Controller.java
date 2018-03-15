08c3331172fb4e683162695f016ff2997623cfd1
A.R. Maged <abdulrahman.maged95@gmail.com>
2018-03-16 00:49:53 +0200

    :hammer: Add new methods to users service controller

--- src/main/java/services/users/Controller.java
+++ src/main/java/services/users/Controller.java
@@ -1,11 +1,16 @@
 package services.users;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
+import persistence.sql.users.User;
 
 import java.io.IOException;
+import java.util.List;
 
 import static persistence.sql.Main.closeConnection;
 import static persistence.sql.Main.openConnection;
+import static persistence.sql.users.Main.getUsersByIds;
+import static persistence.sql.users.Main.getUsersIdsByUsernames;
 
 
 public class Controller extends shared.MQServer.Controller {
@@ -44,8 +49,26 @@ public class Controller extends shared.MQServer.Controller {
                 break;
             case "authorizedToView":
                 resJSON = Authentication.authorizedToView(paramsObject.getString("viewerId"), paramsObject.getString("toBeViewedId"));
+                break;
+            case "getUsersByIds":
+                // TODO @maged: Refactor logic into dedicated file
+                List<User> users = getUsersByIds(
+                        new String[] {"000b332b-cd4e-afef-b58e-af7feab727eb"} // TODO @magdy: Swap with data from params object
+                );
+                resJSON = new JSONObject()
+                        .put("response", new JSONObject().put("data", new JSONArray(users)));
+                break;
+            case "getUsersIdsByUsernames":
+                // TODO @maged: Refactor logic into dedicated file
+                List<String> ids = getUsersIdsByUsernames(
+                        new String[] {} // TODO @magdy: Swap with data from params object
+                );
+                resJSON = new JSONObject()
+                        .put("response", new JSONObject().put("data", new JSONArray(ids)));
+                break;
         }
         closeConnection();
+        System.out.println(resJSON);
         return resJSON;
     }
 }
