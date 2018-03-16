dee72e34009d682476558c926245e9d141ba3364
A.R. Maged <abdulrahman.maged95@gmail.com>
2018-03-16 11:57:41 +0200

    :rewind: Revert non-users-service related changes

--- src/main/java/services/activities/Controller.java
+++ src/main/java/services/activities/Controller.java
@@ -4,49 +4,17 @@ import org.json.JSONObject;
 
 public class Controller extends shared.MQServer.Controller {
 
-    public Controller() {
-        super();
-    }
 
-    @Override
-    public JSONObject execute(JSONObject jsonObject, String userId) {
-        JSONObject newJsonObj = new JSONObject();
-        newJsonObj.put("application", "activities");
-        String methodName = jsonObject.getString("method");
-		JSONObject paramsObject = jsonObject.getJSONObject("params");
-        
-        //interface insert method, change params of json object to match different activity
-        //types
-        switch (methodName) {
-		case "createPostWithTag":
-			NotificationActions.handlePostTagNotification(paramsObject,userId);
-			break;
-		case "createPostLike":
-			NotificationActions.handlePostLikeNotification(paramsObject, userId);
-			break;
-		case "createComment":
-			NotificationActions.handleCommentNotification(paramsObject, userId);
-			break;	
-		case "createCommentLike":
-			NotificationActions.handleCommentNotification(paramsObject, userId);
-			break;
-		case "createCommentReply":
-			NotificationActions.handleCommentReplyNotification(paramsObject, userId);
-			break;
-		case "createFollow":
-			NotificationActions.handleFollowNotification(paramsObject,userId);
-			break;
-		case "getNotifications":
-			NotificationActions.handleGettingNotifications(userId);
-			break;
-		case "getActivities":
-			ActivityActions.handleGettingActivities(userId);
-			break;
-			
-		default:
-			break;
-		}
-        return newJsonObj;
-    }
+	public Controller() {
+		super();
+	}
+
+	@Override
+	public JSONObject execute(JSONObject jsonObject, String userId) {
+		JSONObject newJsonObj = new JSONObject();
+		newJsonObj.put("application", "activities");
+		System.out.println(jsonObject);
+		return newJsonObj;
+	}
 
 }
