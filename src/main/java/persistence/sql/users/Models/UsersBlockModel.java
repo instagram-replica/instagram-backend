370abdfca15ce1ae09b3a29565b44b6df06b5e63
A.R. Maged <abdulrahman.maged95@gmail.com>
2018-03-19 21:31:47 +0200

    :fire: Nuke unused models

--- src/main/java/persistence/sql/users/Models/UsersBlockModel.java
+++
@@ -1,7 +0,0 @@
-package persistence.sql.users.Models;
-
-import org.javalite.activejdbc.annotations.Table;
-
-@Table("users_blocks")
-public class UsersBlockModel extends org.javalite.activejdbc.Model {
-}
