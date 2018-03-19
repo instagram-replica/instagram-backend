b485862381efc64e69444825956026b10ed6e800
Ahmed Elhanafy <ahmed.elhanafy95@gmail.com>
2018-03-19 19:08:02 +0200

    :wrench: Create properties file for redis

--- src/main/java/persistence/cache/Cache.java
+++ src/main/java/persistence/cache/Cache.java
@@ -6,151 +6,157 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
+import java.io.IOException;
+import java.util.Properties;
 import java.util.Set;
 
+import static utilities.Main.readPropertiesFile;
+
 public class Cache {
+    private static Properties properties;
+    public static final int EXPIRY_TIME = 1;
 
-    public static final int EXPIRY_TIME = 3600;
-    public static final String RedisHost = "localhost";
+    static {
+        try {
+            properties = readPropertiesFile("src/main/resources/redis.properties");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
-    public static void insertPostIntoCache(JSONObject post, String postId){
+    public static void insertPostIntoCache(JSONObject post, String postId) {
         System.out.println("INSERTING INTO CACHE");
-        JedisPool pool = new JedisPool(new JedisPoolConfig(), RedisHost);
+        JedisPool pool = new JedisPool(new JedisPoolConfig(), properties.getProperty("host"), Integer.parseInt(properties.getProperty("port")));
         try (Jedis jedis = pool.getResource()) {
-            String key = "post$"+postId;
-            jedis.set(key,post.toString());
-            jedis.expire(key,EXPIRY_TIME);
+            String key = "post$" + postId;
+            jedis.set(key, post.toString());
+            jedis.expire(key, EXPIRY_TIME);
         }
         pool.close();
     }
 
-    public static JSONObject getPostFromCache(String postId){
+    public static JSONObject getPostFromCache(String postId) {
         System.out.println("READING FROM CACHE");
-        JedisPool pool = new JedisPool(new JedisPoolConfig(), RedisHost);
+        JedisPool pool = new JedisPool(new JedisPoolConfig(), properties.getProperty("host"), Integer.parseInt(properties.getProperty("port")));
         try (Jedis jedis = pool.getResource()) {
-            String key = "post$"+postId;
+            String key = "post$" + postId;
             String jsonPost = jedis.get(key);
             pool.close();
-            if(jsonPost!=null){
+            if (jsonPost != null) {
                 return new JSONObject(jsonPost);
-            }
-            else{
+            } else {
                 return null;
             }
         }
     }
 
-    public static void insertPostsIntoCache(JSONArray post, String userId, int pageIndex, int pageSize){
+    public static void insertPostsIntoCache(JSONArray post, String userId, int pageIndex, int pageSize) {
         System.out.println("INSERTING INTO CACHE");
-        JedisPool pool = new JedisPool(new JedisPoolConfig(), RedisHost);
+        JedisPool pool = new JedisPool(new JedisPoolConfig(), properties.getProperty("host"), Integer.parseInt(properties.getProperty("port")));
         try (Jedis jedis = pool.getResource()) {
-            String key = "posts$"+userId+"$"+pageIndex+"$"+pageSize;
-            jedis.set(key,post.toString());
-            jedis.expire(key,EXPIRY_TIME);
+            String key = "posts$" + userId + "$" + pageIndex + "$" + pageSize;
+            jedis.set(key, post.toString());
+            jedis.expire(key, EXPIRY_TIME);
         }
         pool.close();
     }
 
-    public static JSONArray getPostsFromCache(String userId, int pageIndex, int pageSize){
-        System.out.println("READING FROM CACHE");
-        JedisPool pool = new JedisPool(new JedisPoolConfig(), RedisHost);
+    public static JSONArray getPostsFromCache(String userId, int pageIndex, int pageSize) {
+        JedisPool pool = new JedisPool(new JedisPoolConfig(), properties.getProperty("host"), Integer.parseInt(properties.getProperty("port")));
         try (Jedis jedis = pool.getResource()) {
-            String key = "posts$"+userId+"$"+pageIndex+"$"+pageSize;
+            String key = "posts$" + userId + "$" + pageIndex + "$" + pageSize;
             String jsonPosts = jedis.get(key);
             pool.close();
-            if(jsonPosts!=null){
+            if (jsonPosts != null) {
+                System.out.println("READING FROM CACHE");
                 return new JSONArray(jsonPosts);
-            }
-            else{
+            } else {
                 return null;
             }
         }
     }
-    public static void insertCommentsIntoCache(JSONArray comments, String postId){
+
+    public static void insertCommentsIntoCache(JSONArray comments, String postId) {
         System.out.println("INSERTING INTO CACHE");
-        JedisPool pool = new JedisPool(new JedisPoolConfig(), RedisHost);
+        JedisPool pool = new JedisPool(new JedisPoolConfig(), properties.getProperty("host"), Integer.parseInt(properties.getProperty("port")));
         try (Jedis jedis = pool.getResource()) {
-            String key = "comments$"+postId;
-            jedis.set(key,comments.toString());
-            jedis.expire(key,EXPIRY_TIME);
+            String key = "comments$" + postId;
+            jedis.set(key, comments.toString());
+            jedis.expire(key, EXPIRY_TIME);
         }
         pool.close();
     }
 
-    public static JSONArray getCommentsFromCache(String postId){
+    public static JSONArray getCommentsFromCache(String postId) {
         System.out.println("READING FROM CACHE");
-        JedisPool pool = new JedisPool(new JedisPoolConfig(), RedisHost);
+        JedisPool pool = new JedisPool(new JedisPoolConfig(), properties.getProperty("host"), Integer.parseInt(properties.getProperty("port")));
         try (Jedis jedis = pool.getResource()) {
-            String key = "comments$"+postId;
+            String key = "comments$" + postId;
             String jsonComments = jedis.get(key);
             pool.close();
-            if(jsonComments!=null){
+            if (jsonComments != null) {
                 return new JSONArray(jsonComments);
-            }
-            else{
+            } else {
                 return null;
             }
         }
     }
 
 
-
-    public static void insertUserStoriesIntoCache(JSONArray stories, String userId){
+    public static void insertUserStoriesIntoCache(JSONArray stories, String userId) {
         System.out.println("INSERTING INTO CACHE");
-        JedisPool pool = new JedisPool(new JedisPoolConfig(), RedisHost);
+        JedisPool pool = new JedisPool(new JedisPoolConfig(), properties.getProperty("host"), Integer.parseInt(properties.getProperty("port")));
         try (Jedis jedis = pool.getResource()) {
-            String key = "userstories$"+userId;
-            jedis.set(key,stories.toString());
-            jedis.expire(key,EXPIRY_TIME);
+            String key = "userstories$" + userId;
+            jedis.set(key, stories.toString());
+            jedis.expire(key, EXPIRY_TIME);
         }
         pool.close();
     }
 
-    public static JSONArray getUserStoriesFromCache(String userId){
+    public static JSONArray getUserStoriesFromCache(String userId) {
         System.out.println("READING FROM CACHE");
-        JedisPool pool = new JedisPool(new JedisPoolConfig(), RedisHost);
+        JedisPool pool = new JedisPool(new JedisPoolConfig(), properties.getProperty("host"), Integer.parseInt(properties.getProperty("port")));
         try (Jedis jedis = pool.getResource()) {
-            String key = "userstories$"+userId;
+            String key = "userstories$" + userId;
             String jsonStories = jedis.get(key);
             pool.close();
-            if(jsonStories!=null){
+            if (jsonStories != null) {
                 return new JSONArray(jsonStories);
-            }
-            else{
+            } else {
                 return null;
             }
         }
     }
 
 
-    public static void insertStoryIntoCache(JSONObject story, String id){
+    public static void insertStoryIntoCache(JSONObject story, String id) {
         System.out.println("INSERTING INTO CACHE");
-        JedisPool pool = new JedisPool(new JedisPoolConfig(), RedisHost);
+        JedisPool pool = new JedisPool(new JedisPoolConfig(), properties.getProperty("host"), Integer.parseInt(properties.getProperty("port")));
         try (Jedis jedis = pool.getResource()) {
-            String key = "story$"+id;
-            jedis.set(key,story.toString());
-            jedis.expire(key,EXPIRY_TIME);
+            String key = "story$" + id;
+            jedis.set(key, story.toString());
+            jedis.expire(key, EXPIRY_TIME);
         }
         pool.close();
     }
 
 
-    public static JSONObject getStoryFromCache(String id){
+    public static JSONObject getStoryFromCache(String id) {
         System.out.println("READING FROM CACHE");
-        JedisPool pool = new JedisPool(new JedisPoolConfig(), RedisHost);
+        JedisPool pool = new JedisPool(new JedisPoolConfig(), properties.getProperty("host"), Integer.parseInt(properties.getProperty("port")));
         try (Jedis jedis = pool.getResource()) {
-            String key = "story$"+id;
+            String key = "story$" + id;
             String jsonStory = jedis.get(key);
             pool.close();
-            if(jsonStory!=null){
+            if (jsonStory != null) {
                 return new JSONObject(jsonStory);
-            }
-            else{
+            } else {
                 return null;
             }
         }
     }
-    
+
 
 }
