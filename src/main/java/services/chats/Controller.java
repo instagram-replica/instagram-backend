dc4d744896ca65729e44d1b4005d54855500eca8
Ahmed Sawahel <swahel94@gmail.com>
2018-03-20 17:13:29 +0200

    :baby_chick: messenger app logic init

--- src/main/java/services/chats/Controller.java
+++ src/main/java/services/chats/Controller.java
@@ -12,14 +12,27 @@ public class Controller extends shared.mq_server.Controller {
     @Override
     public JSONObject execute(JSONObject jsonObject, String userId) {
         JSONObject newJsonObj = new JSONObject();
-        if(jsonObject.getString("name").equals("Ants")) {
-            try {
-                Thread.sleep(100000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        newJsonObj.put("application", "activities");
+        String methodName = jsonObject.getString("method");
+        JSONObject paramsObject = jsonObject.getJSONObject("params");
+
+        //interface insert method, change params of json object to match different activity
+        //types
+        switch (methodName) {
+            case "createMessage":
+                newJsonObj = Messenger.createMessage(paramsObject, userId);
+                break;
+            case "createThread":
+                newJsonObj = Messenger.createThread(paramsObject, userId);
+                break;
+            case "getMessages":
+                newJsonObj = Messenger.getMessages(paramsObject, userId);
+            case "getThreads":
+                newJsonObj = Messenger.getThreads(paramsObject, userId);
+                break;
+            default:
+                break;
         }
-        newJsonObj.put("application", "chats");
         return newJsonObj;
     }
 }
---
+++ src/main/java/services/chats/Messenger.java
@@ -0,0 +1,21 @@
+package services.chats;
+
+import org.json.JSONObject;
+
+public class Messenger {
+    public static JSONObject createMessage(JSONObject params, String userId){
+        return null;
+    }
+
+    public static JSONObject createThread(JSONObject paramsObject, String userId) {
+        return null;
+    }
+
+    public static JSONObject getMessages(JSONObject paramsObject, String userId) {
+        return null;
+    }
+
+    public static JSONObject getThreads(JSONObject paramsObject, String userId) {
+        return null;
+    }
+}
