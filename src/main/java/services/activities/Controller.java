146cb0eb7526ac04dd9e6c22962f8b2a4183ffde
Mostafa El-Messiry <mostafamesiry@gmail.com>
2018-03-20 23:41:26 +0200

    merging with master

--- src/main/java/services/activities/Controller.java
+++ src/main/java/services/activities/Controller.java
@@ -11,41 +11,41 @@ public class Controller extends shared.mq_server.Controller {
     @Override
     public JSONObject execute(JSONObject jsonObject, String userId) {
         JSONObject newJsonObj = new JSONObject();
-        newJsonObj.put("application", "activities");
+
         String methodName = jsonObject.getString("method");
-		JSONObject paramsObject = jsonObject.getJSONObject("params");
-        
+        JSONObject paramsObject = jsonObject.getJSONObject("params");
+
         //interface insert method, change params of json object to match different activity
         //types
         switch (methodName) {
-		case "createPostWithTag":
-			NotificationActions.handlePostTagNotification(paramsObject,userId);
-			break;
-		case "createPostLike":
-			NotificationActions.handlePostLikeNotification(paramsObject, userId);
-			break;
-		case "createComment":
-			NotificationActions.handleCommentNotification(paramsObject, userId);
-			break;	
-		case "createCommentLike":
-			NotificationActions.handleCommentNotification(paramsObject, userId);
-			break;
-		case "createCommentReply":
-			NotificationActions.handleCommentReplyNotification(paramsObject, userId);
-			break;
-		case "createFollow":
-			NotificationActions.handleFollowNotification(paramsObject, userId);
-			break;
-		case "getNotifications":
-			newJsonObj = NotificationActions.handleGettingNotifications(paramsObject, userId);
-			break;
-		case "getActivities":
-			newJsonObj = ActivityActions.handleGettingActivities(paramsObject, userId);
-			break;
-			
-		default:
-			break;
-		}
+            case "createPostWithTag":
+                NotificationActions.handlePostTagNotification(paramsObject, userId);
+                break;
+            case "createPostLike":
+                NotificationActions.handlePostLikeNotification(paramsObject, userId);
+                break;
+            case "createComment":
+                NotificationActions.handleCommentNotification(paramsObject, userId);
+                break;
+            case "createCommentLike":
+                NotificationActions.handleCommentNotification(paramsObject, userId);
+                break;
+            case "createCommentReply":
+                NotificationActions.handleCommentReplyNotification(paramsObject, userId);
+                break;
+            case "createFollow":
+                NotificationActions.handleFollowNotification(paramsObject, userId);
+                break;
+            case "getNotifications":
+                newJsonObj = NotificationActions.handleGettingNotifications(paramsObject, userId);
+                break;
+            case "getActivities":
+                newJsonObj = ActivityActions.handleGettingActivities(paramsObject, userId);
+                break;
+
+            default:
+                break;
+        }
         return newJsonObj;
     }
 
